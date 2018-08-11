61ee1d0f0dc8286fa6a9d37d41acc931b41fb690
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 06:06:09 +0000

    automatically backed up by learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -4,6 +4,10 @@ class Spaceship {
     this.name = name
     this.crewArray = crewArray
     this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    
 
   }
 
@@ -13,24 +17,6 @@ class Spaceship {
 }
 
 
-describe('spaceShip.js', () => {
-  describe('Spaceship', () => {
-    let spaceship;
-    beforeEach(() => {
-      spaceship = new Spaceship('The Krestel', [], 5, 4);
-    });
-
-    it('should know its own name', () => {
-      expect(spaceship.name).to.eq('The Krestel');
-    });
-
-    it('should have the correct number of phasers (5)', () => {
-      expect(spaceship.phasers).to.eq(5);
-    });
-
-    it('should have the correct layer of shields (4)', () => {
-      expect(spaceship.shields).to.eq(4);
-    });
 
     it('should have its cloaking set to false by default', () => {
       expect(spaceship.cloaked).to.eq(false);
