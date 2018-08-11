cef1e0d69b2555ac324477d252652fa2e68a7ed7
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 06:45:49 +0000

    automatically backed up by learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -3,7 +3,7 @@ class CrewMember {
 
   constructor(position) {
     this.position = position
-    this.currentShip === undefined
+    this.currentShip = undefined
     if (this.currentShip === undefined) {
       this.currentShip = 'Looking for a Rig'
     }
