08bec7611d751aaace2b8acd94f1a736822a8cea
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 07:10:08 +0000

    automatically backed up by learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -23,53 +23,53 @@ class CrewMember {
   }
 }
 
-// class CrewMember{
-//
-//
-//   constructor(position) {
-//     this.position = position
-//
-//       this.currentShip = 'Looking for a Rig'
-//
-//     }
-//
-//     engageWarpDrive () {
-//       if (this.position === 'Pilot' && this.currentShip === 'Looking for a Rig') {
-//         return 'had no effect'
-//       }
-//       if (this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig') {
-//         this.currentShip.warpDrive = 'engaged'
-//
-//       }
-//
-//     }
-//
-//     setsInvisibility () {
-//       if (this.position === 'Defender' && this.currentShip === 'Looking for a Rig') {
-//         return 'had no effect'
-//       }
-//       if (this.position === 'Defender' && this.currentShip !== 'Looking for a Rig') {
-//         this.currentShip.cloaked = true
-//
-//       }
-//     }
-//
-//     chargePhasers () {
-//       if (this.position === 'Gunner' && this.currentShip === 'Looking for a Rig') {
-//         return 'had no effect'
-//       } else {
-//
-//       }
-//     }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//   }
+class CrewMember{
+
+
+  constructor(position) {
+    this.position = position
+
+      this.currentShip = 'Looking for a Rig'
+
+    }
+
+    engageWarpDrive () {
+      if (this.position === 'Pilot' && this.currentShip === 'Looking for a Rig') {
+        return 'had no effect'
+      }
+      if (this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig') {
+        this.currentShip.warpDrive = 'engaged'
+
+      }
+
+    }
+
+    setsInvisibility () {
+      if (this.position === 'Defender' && this.currentShip === 'Looking for a Rig') {
+        return 'had no effect'
+      }
+      if (this.position === 'Defender' && this.currentShip !== 'Looking for a Rig') {
+        this.currentShip.cloaked = true
+
+      }
+    }
+
+    chargePhasers () {
+      if (this.position === 'Gunner' && this.currentShip === 'Looking for a Rig') {
+        return 'had no effect'
+      } else {
+
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+  }
