b14d9db5b7b6865c0ba872f0c7c6c8762b2a82bc
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 06:37:26 +0000

    automatically backed up by learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -3,34 +3,15 @@ class CrewMember {
 
   constructor(position) {
     this.position = position
-
-
     if (this.currentShip === undefined) {
       this.currentShip = 'Looking for a Rig'
     }
-
-
-
-    if (this.position === 'Pilot' && this.currentShip === undefined) {
-      this.engageWarpDrive = function () {
-        return 'had no effect'
-      }
-    }
-    if (this.position === 'Defender' && this.currentShip === undefined) {
-      this.setsInvisibility = function () {
-        return 'had no effect'
-      }
-    }
-    if (this.position === 'Gunner' && this.currentShip === undefined) {
-      this.chargePhasers = function () {
-        return 'had no effect'
-      }
     }
 
-    }
+    
 
 
-  
+
 
 
 
