defd267d349a9e38161dea69849c92e18547424e
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 06:35:28 +0000

    automatically backed up by learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -6,9 +6,9 @@ class CrewMember {
 
     }
 
-    // if (this.currentShip === undefined) {
-    //   this.currentShip = 'Looking for a Rig'
-    // }
+    if (this.currentShip === undefined) {
+      this.currentShip = 'Looking for a Rig'
+    }
     // if (this.position === 'Pilot' && this.currentShip === undefined) {
     //   this.engageWarpDrive = function () {
     //     return 'had no effect'
