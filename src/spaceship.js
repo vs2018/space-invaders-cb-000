e1d38a7a5af62b5161cd5516ff3efa0be12b7ca9
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 07:09:18 +0000

    automatically backed up by learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,9 +1,26 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    crew.length ? (this.docked = false) : (this.docked = true);
+    this.notifyCrew();
+  }
+
+  notifyCrew() {
+    this.crew.forEach(crewMember => (crewMember.currentShip = this));
+  }
+}
 // class Spaceship {
-// 
+//
 //   constructor(name, crewArray, phasers, shields) {
 //     this.name = name
 //     this.crewArray = crewArray
-// 
+//
 //     this.phasers = phasers
 //     this.shields = shields
 //     this.cloaked = false
@@ -15,17 +32,17 @@
 //     }
 //     this.phasersCharge = 'uncharged'
 //     this.notifyCrew();
-// 
-// 
+//
+//
 //   }
-// 
+//
 //   notifyCrew() {
 //   this.crewArray.forEach(crewMember => (crewMember.currentShip = this));
 // }
-// 
-// 
-// 
-// 
-// 
-// 
+//
+//
+//
+//
+//
+//
 // }
