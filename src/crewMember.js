3e1c99bdb4add9371727f2d1c81e4e44dbc0ab53
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 06:12:28 +0000

    automatically backed up by learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -14,13 +14,13 @@ class CrewMember {
 }
 
 
--const expect = chai.expect;
+// -const expect = chai.expect;
 
 
 //   it("should return 'Looking for a Rig' if they aren't assigned to a ship", () => {
 //     expect(tristan.currentShip).to.match(/Looking for a Rig/i);
 //   });
-// 
+//
 //   it("should return 'had no effect' when the crew member tries to use their special ability when not assigned to a ship", () => {
 //     expect(tristan.engageWarpDrive()).to.match(/had no effect/i);
 //     expect(jon.setsInvisibility()).to.match(/had no effect/i);
